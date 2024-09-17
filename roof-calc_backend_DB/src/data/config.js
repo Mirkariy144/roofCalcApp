6d5e520a9903661d50d4ede6d12304991c83ec55
Mirkariy144 <Syam144@yandex.ru>
2024-09-17 23:19:00 +0300

    Update config.js

--- roof-calc_backend_DB/src/data/config.js
+++ roof-calc_backend_DB/src/data/config.js
@@ -2,15 +2,13 @@ const mysql = require('mysql2');
 
 // Set database connection credentials
 const config = {
-  host: '172.18.0.2',
+  host: 'db',
   user: 'root',
   password: 'root',
   database: 'sql_roof-calc',
   port: 3306,
   authPlugin: 'mysql_native_password',
-  ssl: {
-    rejectUnauthorized: false
-  }
+  rejectUnauthorized: false,
 };
 
 // const connection = mysql.createConnection({
