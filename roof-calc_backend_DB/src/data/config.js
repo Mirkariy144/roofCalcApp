a77413899f4d58ec38113e66875264dd6b1bc6b7
Mirkariy144 <Syam144@yandex.ru>
2024-09-18 00:04:59 +0300

    Update config.js

--- roof-calc_backend_DB/src/data/config.js
+++ roof-calc_backend_DB/src/data/config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // Set database connection credentials
 const config = {
-  host: '%',
+  host: 'localhost',
   user: 'kurwasql',
   password: 'kurwasql2',
   database: 'sql_roof-calc',
