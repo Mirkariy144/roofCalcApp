d02ffd51f95dbf62e57c6405ee06781fc4d20458
Mirkariy144 <Syam144@yandex.ru>
2024-09-19 22:16:00 +0300

    Update config.js

--- roof-calc_backend_DB/src/data/config.js
+++ roof-calc_backend_DB/src/data/config.js
@@ -23,11 +23,11 @@ const config = {
 //   port: 3306
 // });
 
-const pool = mysql.createConnection(config);
+// const pool = mysql.createConnection(config);
 
 // connection.config.authPlugin = 'mysql_native_password';
 
 // Create a MySQL pool
-// const pool = mysql.createPool(config);
+const pool = mysql.createPool(config);
 
 module.exports = pool;
