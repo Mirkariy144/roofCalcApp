434e3076eec213c794cbe6f86470e98b90f98676
Mirkariy144 <Syam144@yandex.ru>
2024-09-30 19:25:16 +0300

    Update webpack.config.js

--- roof_calc_frontend/webpack.config.js
+++ roof_calc_frontend/webpack.config.js
@@ -28,11 +28,15 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader",
-          "css-loader",
-           {
-            'loader': 'css-loader',
-             options: { importLoaders: 1 }}],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
       },
       {
         test: /\.module\.css$/,
